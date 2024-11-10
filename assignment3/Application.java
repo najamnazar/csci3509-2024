f94aa69b01bad6f3e422da2394cafb2d60568400
Najam Nazar <najamnazar@gmail.com>
2024-11-11 00:24:03 +0400

    starter code

---
+++ assignment3/Application.java
@@ -0,0 +1,15 @@
+package assignment3;
+
+/*
+ * @author Najam
+ * 
+ * This is a client class that calls required methods
+ * from other classes.
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Parallax Real Estate Application!");
+        // Students should write client code here...
+	}
+}
---
+++ assignment3/models/Buyer.java
@@ -0,0 +1,14 @@
+package assignment3.models;
+
+/**
+ * 
+ * @author najam
+ * Pojo class
+ * Class definition should be added here
+ *
+ */
+public class Buyer {
+	private String buyerId;
+	// add remaining code here.
+	
+}
